be8425db76ba2d4d3d3db8a327fc9747659db183
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-05-23 15:24:32 -0300

    Refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.client import MCPClient
 import asyncio
+import sys
 
 
 async def main():
@@ -8,13 +9,14 @@ async def main():
         sys.exit(1)
 
     client = MCPClient()
+
     try:
         await client.connect_to_server(sys.argv[1])
+
         await client.chat_loop()
     finally:
         await client.cleanup()
 
 
 if __name__ == "__main__":
-    import sys
     asyncio.run(main())
